b82ca7c08642a14ac94842eadb2d3653493db538
touchBar <brianr7400@gmail.com>
2018-11-12 21:02:14 -0500

    Started hw10pr1.py

--- HW10/hw10pr1.py
+++ HW10/hw10pr1.py
@@ -4,3 +4,30 @@ def readFile(fileName):
     text = inFile.read()
     inFile.close()
     return text
+
+def readFileFor(fileName):
+    inFile = open(fileName, 'r')
+    total = 0
+
+    for line in inFile:
+        lineList = line.split()
+        total = total + int(lineList[2])
+
+    inFile.close()
+    return total
+
+def readFileWhile(fileName):
+    inFile = open(fileName, 'r')
+    total = 0
+
+    while True:
+        line = inFile.readLine()
+        if not line:
+            break
+        lineList = line.split()
+        total += int(lineList[2])
+
+    inFile.close()
+    return total
+
+
