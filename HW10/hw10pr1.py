4836284c860219ff1fa3ce3a7d6016f30f599a16
touchBar <brianr7400@gmail.com>
2018-11-13 19:39:08 -0500

    hw10pr1 fully functional

--- HW10/hw10pr1.py
+++ HW10/hw10pr1.py
@@ -42,31 +42,40 @@ def plotDrugUse():
     #Load csv
     f = open('drug_use_age.csv','r')
     lines = f.read().split('\n')
-    table = [[] for _ in range(len(lines))] #table[col][row]
-    print(table)
-    for line in lines:
-        line = line.split(',') #Line is array of values
-        for col, val in enumerate(line):
-            table[col].append(val)
-
+    lines = lines[:-1] #Removes blank line at end
+    #table = [[] for _ in range(len(lines))] #table[col][row]
+    table = [[header] for header in lines[0].split(',')]
+    print(table,'\n',len(table))
+    for line in lines[1:]:
+        values = line.split(',')
+        print('len of values: ', len(values))
+        for col,e in enumerate(values):
+            print(col,e)
+            if e == '-':
+                e = 0
+            if col >= 1:
+                table[col].append(float(e))
+            else:
+                table[col].append(e)
     #Process into numpy
     x_labels = table[0][1:]
-    x = range(x_labels)
+    print('x_labels\n',x_labels)
+    x = range(len(x_labels))
     ganja_use = [e for e in table[4][1:]]
     drank_use = [e for e in table[2][1:]]
 
-    plt.xticks(x,x_labels,rotation='horizontal')
+    plt.margins(0.2)
+    plt.subplots_adjust(bottom=0.15)
+    plt.plot(x,drank_use,'r-',label='Alcohol Usage')
+    plt.plot(x,ganja_use,'--',label='Marijuana Usage')
+    plt.xticks(x,x_labels,rotation='vertical')
     plt.xlim([0,len(x_labels)-1])
-    plt.ylim([0,100])
+    plt.ylim(0,100)
+    plt.xlabel("Age Groups")
+    plt.ylabel("Percentage of age group who used drug in past 12 months")
 
-    plt.title("Comparitive usages of Marijuana and Alcohol in age-bracket")
-    plt.xlabel("Age-bracket")
-    plt.ylabel("Percent use")
-
+    plt.title("From national survey on drug use 2015")
     plt.legend(loc = 'upper right', shadow = True)
-
-    plt.plot(x,ganja_use,'--',label=("Marijuana Usage"))
-    plt.plot(x,drank_use,'--',label=("Alcohol Usage"))
 
     plt.show()
 
